85bb82af2b23795f04b4ddeaa8a4be007cbca607
Dennis-code1 <aloysiousdennis90@gmail.com>
2026-08-12 10:34:24 +0000

    I worked on creating a checkout, and with CSS

---
+++ food.js
@@ -0,0 +1,16 @@
+<script>
+     function showSignup()
+     {
+      document.getElementById("loginForm")
+      .classList.add("hidden");
+      document.getElementById("signupForm")
+      .classList.remove("hidden");
+      }
+      function showLogin()
+      {
+      document.getElementById("signupForm")
+      .classList.add("hidden");
+      document.getElementById("loginForm")
+      .classList.remove("hidden");
+      }
+      </script>
